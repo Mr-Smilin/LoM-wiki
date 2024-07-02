c6b4e6e04158c13dcb6625d3704a410bb04009ca
Mr-Smilin <lion31lion31@gmail.com>
2024-07-02 22:00:02 +0800

    更新貢獻者列表 手動新增 PR 人員

--- tools/buildContributors.js
+++ tools/buildContributors.js
@@ -15,7 +15,19 @@ axios
 			links: [{ icon: "github", link: contributor.html_url }],
 		}));
 
-		fs.writeFileSync(outputFile, JSON.stringify(contributors, null, 2));
+		// PR 貢獻者抓不到，對 API 資源占用過高
+		const manualContributors = [
+			{
+				avatar: "https://www.github.com/pon50895.png",
+				name: "pon50895",
+				title: "Contributor via PR",
+				links: [{ icon: "github", link: "https://github.com/pon50895" }],
+			},
+		];
+
+		const allContributors = [...contributors, ...manualContributors];
+
+		fs.writeFileSync(outputFile, JSON.stringify(allContributors, null, 2));
 		console.log("Contributors list has been generated!");
 	})
 	.catch((error) => {
