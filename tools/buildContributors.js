3bf708980e96902454b11510ba0723c58962d74d
Smilin <lion31lion31@gmail.com>
2024-07-03 10:09:17 +0800

    調整列表邏輯 更新貢獻者名單 表情符號分類 導覽更新

--- tools/buildContributors.js
+++ tools/buildContributors.js
@@ -6,30 +6,31 @@ const repo = "LoM-wiki";
 const outputFile = "docs/public/json/contributors.json";
 
 axios
-	.get(`https://api.github.com/repos/${owner}/${repo}/contributors`)
-	.then((response) => {
-		const contributors = response.data.map((contributor) => ({
-			avatar: contributor.avatar_url,
-			name: contributor.login,
-			title: "Contributor",
-			links: [{ icon: "github", link: contributor.html_url }],
-		}));
+  .get(`https://api.github.com/repos/${owner}/${repo}/contributors`)
+  .then((response) => {
+    const contributors = response.data.map((contributor) => ({
+      avatar: contributor.avatar_url,
+      name: contributor.login,
+      title: "Contributor",
+      links: [{ icon: "github", link: contributor.html_url }],
+    }));
 
-		// PR 貢獻者抓不到，對 API 資源占用過高
-		const manualContributors = [
-			{
-				avatar: "https://www.github.com/pon50895.png",
-				name: "pon50895",
-				title: "Contributor via PR",
-				links: [{ icon: "github", link: "https://github.com/pon50895" }],
-			},
-		];
+    // PR 貢獻者抓不到，對 API 資源占用過高
+    const manualContributors = [
+      // MR 後加入
+      // {
+      // 	avatar: "https://www.github.com/pon50895.png",
+      // 	name: "pon50895",
+      // 	title: "Contributor via PR",
+      // 	links: [{ icon: "github", link: "https://github.com/pon50895" }],
+      // },
+    ];
 
-		const allContributors = [...contributors, ...manualContributors];
+    const allContributors = [...contributors, ...manualContributors];
 
-		fs.writeFileSync(outputFile, JSON.stringify(allContributors, null, 2));
-		console.log("Contributors list has been generated!");
-	})
-	.catch((error) => {
-		console.error("Error fetching contributors:", error);
-	});
+    fs.writeFileSync(outputFile, JSON.stringify(allContributors, null, 2));
+    console.log("Contributors list has been generated!");
+  })
+  .catch((error) => {
+    console.error("Error fetching contributors:", error);
+  });
