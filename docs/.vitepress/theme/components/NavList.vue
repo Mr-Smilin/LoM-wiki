24d0e3b4e4166a7dba562cb8d9d337ac02237b4b
Smilin <lion31lion31@gmail.com>
2024-07-03 16:54:31 +0800

    修復 wiki 導覽沒有加入子網域的問題 美化導覽

--- docs/.vitepress/theme/components/NavList.vue
+++ docs/.vitepress/theme/components/NavList.vue
@@ -1,25 +1,78 @@
 <template>
-	<div>
-		<details v-for="item in navItems" :key="item.text" :open="true">
-			<summary>{{ item.text }}</summary>
-			<ul v-if="item.items">
-				<li v-for="subItem in item.items" :key="subItem.text">
-					<a :href="subItem.link">{{ subItem.text }}</a>
-				</li>
-			</ul>
-			<a v-else :href="item.link">{{ item.text }}</a>
-		</details>
+	<div class="nav-container">
+	  <details v-for="item in navItems" :key="item.text" open>
+		<summary>{{ item.text }}</summary>
+		<ul v-if="item.items" class="sub-items">
+		  <li v-for="subItem in item.items" :key="subItem.text" class="sub-item">
+			<a :href="isString(subItem.link) ? withBase(subItem.link) : '#'">{{ subItem.text }}</a>
+		  </li>
+		</ul>
+		<a v-else :href="isString(item.link) ? withBase(item.link) : '#'">{{ item.text }}</a>
+	  </details>
 	</div>
 </template>
-
+  
 <script>
-import { useData } from "vitepress";
-export default {
+  import { withBase, useData } from "vitepress";
+  
+  export default {
 	setup() {
-		const { theme } = useData();
-		return {
-			navItems: theme.value.nav,
-		};
+	  const { theme } = useData();
+  
+	  const isString = (value) => typeof value === 'string' || value instanceof String;
+  
+	  return {
+		navItems: theme.value.nav,
+		withBase,
+		isString,
+	  };
 	},
-};
+  };
 </script>
+  
+<style scoped>
+  .nav-container {
+	padding: 1rem;
+	background-color: var(--vp-c-bg);
+	border-radius: 8px;
+  }
+  
+  details {
+	margin-bottom: 1rem;
+  }
+  
+  summary {
+	font-weight: bold;
+	cursor: pointer;
+	padding: 0.5rem;
+	background-color: var(--vp-c-bg-alt);
+	border-radius: 4px;
+	transition: background-color 0.3s;
+  }
+  
+  summary:hover {
+	background-color: var(--vp-c-bg-hover);
+  }
+  
+  .sub-items {
+	margin: 0;
+	padding: 0;
+	list-style: none;
+  }
+  
+  .sub-item {
+	margin: 0.5rem 0;
+  }
+  
+  a {
+	text-decoration: none;
+	color: var(--vp-c-brand);
+	transition: color 0.3s, text-decoration 0.3s;
+  }
+  
+  a:hover {
+	text-decoration: underline;
+	color: var(--vp-c-brand-hover);
+  }
+</style>
+  
