40eadfa18f67b373add408c3ec2ef20fdf79d5ab
Smilin <lion31lion31@gmail.com>
2024-07-03 15:46:42 +0800

    新增 tab 元件 修改龍湘文本格式

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,11 +1,17 @@
 // .vitepress/theme/index.js
 import DefaultTheme from "vitepress/theme";
 import MyLayout from "./components/MyLayout.vue";
+import Tabs from "./components/Tabs.vue";
+import Tab from "./components/Tab.vue";
 import "./styles/style.css";
 import "./styles/twikoo.css";
 
 export default {
-	extends: DefaultTheme,
-	// 使用注入插槽的包装组件覆盖 Layout
-	Layout: MyLayout,
+  extends: DefaultTheme,
+  // 使用注入插槽的包装组件覆盖 Layout
+  Layout: MyLayout,
+  enhanceApp({ app }) {
+    app.component("Tabs", Tabs);
+    app.component("Tab", Tab);
+  },
 };
