4d5513e83d2bf1501601f45590a362e99d4db845
roypeng <roypeng@tasys.io>
2024-07-05 04:45:28 +0800

    1. temp commit

--- docs/.vitepress/theme/components/Icon.vue
+++ docs/.vitepress/theme/components/Icon.vue
@@ -1,64 +1,75 @@
 <template>
-  <span class="icon" :style="iconStyle(type)"></span>{{ contents }}
+    <span class="icon" :style="iconStyle(type)"></span>{{ contents }}
 </template>
 
 <script>
-import { withBase } from "vitepress";
+import {withBase} from "vitepress";
+
 export default {
-  props: {
-    type: {
-      type: String,
-      default: 'badend',
-      validator: function (value) {
-        // Only allow specific icon types
-        return [
-            'badend',
-            'be',
-            'default'
-        ].indexOf(value) !== -1
-      }
+    props: {
+        source: {
+            type: String,
+            default: 'badend',
+            validator: function (value) {
+                // Only allow specific icon types
+                return [
+                    'badend',
+                    'default'
+                ].indexOf(value) !== -1
+            }
+        },
+        type: {
+            type: String,
+            default: 'small',
+            validator: function (value) {
+                // Only allow specific icon types
+                return [
+                    'small',
+                    'medium'
+                ].indexOf(value) !== -1
+            }
+        },
+        contents: {
+            type: String,
+            required: false,
+            default: ''
+        }
     },
-    contents: {
-      type: String,
-      required: false,
-      default: ''
-    }
-  },
-  data() {
-    return {
-      basePath: ''
-    };
-  },
-  methods: {
-    iconStyle(iconType) {
-      const TYPE_BADEND = 'badend';
-
-      const ICON_BADEND = 'icon_badend.jpg'
-
-      switch(iconType) {
-        case TYPE_BADEND:
-        case 'be':
-          return {
-          backgroundImage: `url('${withBase(`/images/generic/${ICON_BADEND}`)}')`
+    data() {
+        return {
+            basePath: ''
         };
-        default:
-          return {
-            backgroundImage: `url('${withBase(`/images/generic/${ICON_BADEND}`)}')`
-          };
-      }
+    },
+    methods: {
+        iconStyle(iconType) {
+            const TYPE_BADEND = 'badend';
+
+            const ICON_BADEND = 'icon_badend.jpg'
+
+            switch (iconType) {
+                case TYPE_BADEND:
+                case 'be':
+                    return {
+                        backgroundImage: `url('${withBase(`/images/generic/${ICON_BADEND}`)}')`
+                    };
+                default:
+                    return {
+                        backgroundImage: `url('${withBase(`/images/generic/${ICON_BADEND}`)}')`
+                    };
+            }
+        }
     }
-  }
 };
 </script>
 
 <style scoped>
 .icon {
-  /* Add dimensions to span */
-  display: inline-block;
-  width: 32px;
-  height: 32px;
-  background-repeat: no-repeat;
-  background-size: 100% 100%;
-  vertical-align: top;
+    /* Add dimensions to span */
+    display: inline-block;
+    width: 32px;
+    height: 32px;
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+    vertical-align: top;
 }
 </style>
