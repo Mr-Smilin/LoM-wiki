f8ba533d214bc27be66b2953ebaf32e807487885
roypeng <roypeng@tasys.io>
2024-07-06 02:19:18 +0800

    1. add girl 1,3,4,6,7 icon 2. add Girl[1-8]Icon.vue 3. add icons to dream-sweetheart.md

--- docs/.vitepress/theme/components/Icon.vue
+++ docs/.vitepress/theme/components/Icon.vue
@@ -35,9 +35,26 @@ export default {
         getIconSource(character) {
             const CHAR_BADEND = 'badend';
             const CHAR_GIRL_0  = 'girl0';
+            const CHAR_GIRL_1  = 'girl1';
+            const CHAR_GIRL_2  = 'girl2';
+            const CHAR_GIRL_3  = 'girl3';
+            const CHAR_GIRL_4  = 'girl4';
+            const CHAR_GIRL_5  = 'girl5';
+            const CHAR_GIRL_6  = 'girl6';
+            const CHAR_GIRL_7  = 'girl7';
+            const CHAR_GIRL_8  = 'girl8';
+
             const SOURCE_MAP = {
                 [CHAR_BADEND]: `url('${withBase(`/images/generic/icon_badend.jpg`)}')`,
-                [CHAR_GIRL_0]: `url('${withBase(`/images/characters/girl_0/icon_girl0.png`)}')`
+                [CHAR_GIRL_0]: `url('${withBase(`/images/characters/girl_0/icon_girl0.png`)}')`,
+                [CHAR_GIRL_1]: `url('${withBase(`/images/characters/girl_1/icon_girl1.png`)}')`,
+                [CHAR_GIRL_2]: `url('${withBase(`/images/characters/girl_2/icon_girl2.png`)}')`,
+                [CHAR_GIRL_3]: `url('${withBase(`/images/characters/girl_3/icon_girl3.png`)}')`,
+                [CHAR_GIRL_4]: `url('${withBase(`/images/characters/girl_4/icon_girl4.png`)}')`,
+                [CHAR_GIRL_5]: `url('${withBase(`/images/characters/girl_5/icon_girl5.png`)}')`,
+                [CHAR_GIRL_6]: `url('${withBase(`/images/characters/girl_6/icon_girl6.png`)}')`,
+                [CHAR_GIRL_7]: `url('${withBase(`/images/characters/girl_7/icon_girl7.png`)}')`,
+                [CHAR_GIRL_8]: `url('${withBase(`/images/characters/girl_8/icon_girl8.png`)}')`
             };
 
             // check key exists in map
--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -3,11 +3,20 @@ import DefaultTheme from "vitepress/theme";
 import MyLayout from "./components/MyLayout.vue";
 import Tabs from "./components/Tabs.vue";
 import Tab from "./components/Tab.vue";
-import Icon from "./components/Icon.vue";
 import ChTabs from "./components/CharacterTabs.vue";
 import ChTab from "./components/CharacterTab.vue";
+//import Icon Group
+import Icon from "./components/Icon.vue";
 import BadendIcon from "./components/icons/BadendIcon.vue";
 import Girl0Icon from "./components/icons/Girl0Icon.vue";
+import Girl1Icon from "./components/icons/Girl1Icon.vue";
+import Girl2Icon from "./components/icons/Girl2Icon.vue";
+import Girl3Icon from "./components/icons/Girl3Icon.vue";
+import Girl4Icon from "./components/icons/Girl4Icon.vue";
+import Girl5Icon from "./components/icons/Girl5Icon.vue";
+import Girl6Icon from "./components/icons/Girl6Icon.vue";
+import Girl7Icon from "./components/icons/Girl7Icon.vue";
+import Girl8Icon from "./components/icons/Girl8Icon.vue";
 import "./styles/style.css";
 import "./styles/borderless.css";
 import "./styles/twikoo.css";
@@ -30,5 +39,13 @@ export default {
     // and create a new vue file to register
     app.component("BadendIcon", BadendIcon);
     app.component("Girl0Icon", Girl0Icon);
+    app.component("Girl1Icon", Girl1Icon);
+    app.component("Girl2Icon", Girl2Icon);
+    app.component("Girl3Icon", Girl3Icon);
+    app.component("Girl4Icon", Girl4Icon);
+    app.component("Girl5Icon", Girl5Icon);
+    app.component("Girl6Icon", Girl6Icon);
+    app.component("Girl7Icon", Girl7Icon);
+    app.component("Girl8Icon", Girl8Icon);
   },
 };
