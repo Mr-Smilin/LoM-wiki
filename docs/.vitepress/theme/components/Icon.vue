5aa03e45d03c1f8aec156fa8ba168636bf478741
roypeng <roypeng@tasys.io>
2024-07-05 21:13:09 +0800

    1. modify component icon, for refactor icon component

--- docs/.vitepress/theme/components/Icon.vue
+++ docs/.vitepress/theme/components/Icon.vue
@@ -1,5 +1,5 @@
 <template>
-    <span class="icon" :style="iconStyle(type)"></span>{{ contents }}
+    <span :class=getIconClass(type) :style="getIconSource(character)"></span><slot></slot>
 </template>
 
 <script>
@@ -7,13 +7,14 @@ import {withBase} from "vitepress";
 
 export default {
     props: {
-        source: {
+        character: {
             type: String,
             default: 'badend',
             validator: function (value) {
                 // Only allow specific icon types
                 return [
                     'badend',
+                    'girl0',
                     'default'
                 ].indexOf(value) !== -1
             }
@@ -28,35 +29,47 @@ export default {
                     'medium'
                 ].indexOf(value) !== -1
             }
-        },
-        contents: {
-            type: String,
-            required: false,
-            default: ''
         }
     },
-    data() {
-        return {
-            basePath: ''
-        };
-    },
     methods: {
-        iconStyle(iconType) {
+        getIconSource(character) {
             const TYPE_BADEND = 'badend';
+            const TYPE_GIRL0  = 'girl0';
+            const SOURCE_MAP = {
+                TYPE_BADEND: `url('${withBase(`/images/generic/icon_badend.jpg`)}')`,
+                TYPE_GIRL0: `url('${withBase(`/images/characters/girl_0/icon_girl0.png`)}')`
+            };
 
-            const ICON_BADEND = 'icon_badend.jpg'
+            // check key exists in map
+            if (SOURCE_MAP.hasOwnProperty(character)){
+                return {
+                    backgroundImage: SOURCE_MAP.character
+                };
+            }
+
+            // given default icon the same as TYPE_BADEND
+            return {
+                backgroundImage: SOURCE_MAP.TYPE_BADEND
+            };
+        },
+        getIconClass(iconType) {
+            const TYPE_SMALL = 'small';
+            const TYPE_MEDIUM = 'medium';
+
+            let returnClass = ['icon'];
 
             switch (iconType) {
-                case TYPE_BADEND:
-                case 'be':
-                    return {
-                        backgroundImage: `url('${withBase(`/images/generic/${ICON_BADEND}`)}')`
-                    };
+                case TYPE_SMALL:
+                    returnClass.push(TYPE_SMALL);
+                    break;
+                case TYPE_MEDIUM:
+                    returnClass.push(TYPE_MEDIUM);
+                    break;
                 default:
-                    return {
-                        backgroundImage: `url('${withBase(`/images/generic/${ICON_BADEND}`)}')`
-                    };
+                    returnClass.push(TYPE_SMALL);
+                    break;
             }
+            return returnClass;
         }
     }
 };
@@ -66,10 +79,18 @@ export default {
 .icon {
     /* Add dimensions to span */
     display: inline-block;
-    width: 32px;
-    height: 32px;
     background-repeat: no-repeat;
     background-size: 100% 100%;
     vertical-align: top;
+}
+
+.small {
+    width: 32px;
+    height: 32px;
+}
+
+.medium {
+    width: 100px;
+    height: 100px;
 }
 </style>
