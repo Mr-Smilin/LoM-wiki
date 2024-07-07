80496301c0aacac07cf1550087abbf2c67248928
roypeng <roypeng@tasys.io>
2024-07-08 07:01:02 +0800

    1. modify docs:build command 2. change to using news2.md to experiment

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -78,7 +78,7 @@ export default defineConfig({
 						],
 					},
 					{ text: "唐門圖書館(google sheet)", link: "/other/lom-sheet" },
-					{ text: "官方更新(exp)", link: "/other/news1" },
+					{ text: "官方更新(exp)", link: "/other/news2" },
 				],
 			},
 			{
