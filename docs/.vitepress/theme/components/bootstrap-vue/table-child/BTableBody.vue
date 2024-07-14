d1c39ce4c5fb86c4d17d8e90d8834c87b68cfd45
Mr-Smilin <lion31lion31@gmail.com>
2024-07-14 23:32:24 +0800

    feat: 暫時推上去

---
+++ docs/.vitepress/theme/components/bootstrap-vue/table-child/BTableBody.vue
@@ -0,0 +1,50 @@
+<template>
+    <tbody>
+        <tr v-for="row in rows" :key="row.id" v-bind="row._attributes" v-memo="[row.id, row.isSelected]">
+            <td v-for="(cell, cellIndex) in row._cells" 
+                :key="cellIndex" 
+                v-bind="cell.attributes">
+                <template v-if="cell.type === 'array'">
+                    <template v-for="(item, index) in cell.content" :key="index">
+                        <br v-if="isBreakElement(item)" />
+                        <template v-else-if="isTextElement(item)">{{ item.children }}</template>
+                        <component 
+                        v-else-if="isComponentElement(item)" 
+                        :is="item.type"
+                        v-bind="item.props"
+                        >
+                            <template v-for="(slotContent, slotName) in item.children" :key="slotName" v-slot:[slotName]>
+                                <template v-if="typeof slotContent === 'function'">
+                                    <component :is="{ render: slotContent }" />
+                                </template>
+                                <template v-else>
+                                {{ slotContent }}
+                                </template>
+                            </template>
+                        </component>
+                    </template>
+                </template>
+                <MarkdownWrapper v-else :content="cell.content" />
+            </td>
+        </tr>
+    </tbody>
+</template>
+
+<script setup>
+    const props = defineProps({
+        rows: Array,
+        headers: Array
+    })
+
+    function isBreakElement(item) {
+        return item && item.type === 'br';
+    }
+
+    function isTextElement(item) {
+        return item && typeof item.type === 'symbol' && item.type.description === 'v-txt';
+    }
+
+    function isComponentElement(item) {
+        return item && typeof item.type === 'string' || typeof item.type === 'object';
+    }
+</script>
---
+++ docs/.vitepress/theme/components/bootstrap-vue/table-child/BTableHeader.vue
@@ -0,0 +1,33 @@
+<template>
+    <thead>
+        <tr>
+            <th v-for="header in headers" :key="header.key" @click="handleSort(header)">
+                <span>{{ header.label }}</span>
+                <BTableSortIcon 
+                v-if="!header.unsortable"
+                :sort-key="sortKey"
+                :sort-order="sortOrder"
+                :header-key="header.key"
+                />
+            </th>
+        </tr>
+    </thead>
+</template>
+
+<script setup>
+import BTableSortIcon from './BTableSortIcon.vue'
+
+const props = defineProps({
+    headers: Array,
+    sortKey: String,
+    sortOrder: String
+})
+
+const emit = defineEmits(['sort'])
+
+function handleSort(header) {
+    if (!header.unsortable) {
+        emit('sort', header)
+    }
+}
+</script>
