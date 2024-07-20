6be8c103103983aa1fd0decfb46253c212f7b078
roypeng <roypeng@tasys.io>
2024-07-20 14:03:23 +0800

    1. modify for adding end number limit

--- docs/.vitepress/theme/components/icon-family/icons/EndIcon.vue
+++ docs/.vitepress/theme/components/icon-family/icons/EndIcon.vue
@@ -30,7 +30,7 @@ export default defineComponent({
             type: Number,
             default: 0,
             validator: function (value) {
-                if (value < 0 && value <= 49){
+                if (value < 0 && value <= 50){
                     return true;
                 }
                 return false;
