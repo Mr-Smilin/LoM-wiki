5ebb515a99c13708a0abf99185d498c865734f5b
Mr-Smilin <lion31lion31@gmail.com>
2024-08-12 11:08:51 +0800

    feat: 將主題過度 js 包成獨立檔案

--- docs/.vitepress/theme/components/MyLayout.vue
+++ docs/.vitepress/theme/components/MyLayout.vue
@@ -2,53 +2,22 @@
 <script setup>
 // import { onMounted } from 'vue';
 import DefaultTheme from "vitepress/theme";
-import { useData } from 'vitepress'
-import { defineAsyncComponent, nextTick, provide } from "vue";
+import { defineAsyncComponent, provide } from "vue";
 import Twikoo from "./default/Twikoo.vue";
 import Borderless from "./default/Borderless.vue";
 
 const { Layout } = DefaultTheme;
 const NotFound = defineAsyncComponent(() => import("./default/NotFound.vue"));
-const { isDark } = useData()
+import { useThemeTransition } from '../script/themeTransition.js';
+const { toggleAppearance } = useThemeTransition();
 
 // onMounted(() => {
 // 	import('bootstrap/dist/js/bootstrap.bundle.min.js');
 // 	import('bootstrap-vue/dist/bootstrap-vue.css');
 // });
-const enableTransitions = () =>
-	'startViewTransition' in document &&
-	window.matchMedia('(prefers-reduced-motion: no-preference)').matches
 
 provide('toggle-appearance', async (event) => {
-	const x = event.clientX;
-	const y = event.clientY;
-
-	if (!enableTransitions()) {
-		isDark.value = !isDark.value
-		return
-	}
-
-	const clipPath = [
-		`circle(0px at ${x}px ${y}px)`,
-		`circle(${Math.hypot(
-			Math.max(x, innerWidth - x),
-			Math.max(y, innerHeight - y)
-		)}px at ${x}px ${y}px)`
-	]
-
-	await document.startViewTransition(async () => {
-		isDark.value = !isDark.value
-		await nextTick()
-	}).ready
-
-	document.documentElement.animate(
-		{ clipPath: isDark.value ? clipPath.reverse() : clipPath },
-		{
-			duration: 300,
-			easing: 'ease-in',
-			pseudoElement: `::view-transition-${isDark.value ? 'old' : 'new'}(root)`
-		}
-	)
+	await toggleAppearance(event);
 })
 </script>
 
