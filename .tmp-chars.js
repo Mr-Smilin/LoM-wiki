e897f88a79f0f763a6a46808292ddd6a42204c64
mohui666 <mmmmmohui@gmail.com>
2026-08-10 20:25:38 +0800

    chore: remove accidentally committed temp script

--- .tmp-chars.js
+++
@@ -1,7 +0,0 @@
-const OpenCC = require("opencc-js");
-const convert = OpenCC.Converter({ from: "twp", to: "cn" });
-const candidates = "姪妳牠牀凜啣毘覈蹟卽旣佈嚐牆粵竝尅搥撲閒谿鷄獃娛嬤麽裏穫穗藴蘊慾愾悶吒屛佇彷彿";
-for (const ch of candidates) {
-    const c = convert(ch);
-    if (c === ch) console.log("PASS-THROUGH:", ch);
-}
